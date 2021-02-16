01e06be5ef0e48bd68c0aad1124b8bcb279a8f63
FilouDeNesque <p.cuquemy@yahoo.com>
2021-02-16 14:53:52 +0100

    form contact

--- contact.js
+++ contact.js
@@ -10,3 +10,20 @@ function setvisibmap() {
         visibmap = 0
     }
 }
+
+
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwrrc9I01PQrx8lF1_-oeBroI-AtXXdSvzqp2LAr8RinYayCQf29stq/exec';
+
+$('#submit-form').on('click', function(e){
+    e.preventDefault();
+    var jqxhr = $.ajax({
+        url: url,
+        method:"GET",
+        datatype:"json",
+        data:$form.serializeObject()
+    }).done(function(){
+        //do something
+        console.log($form.serializeObject());
+    });
+})
